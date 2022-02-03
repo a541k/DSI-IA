62ff741e3698bcb249a9a22a5c0001151c2cc2d5
Ashik Mahmud <83023907+a541k@users.noreply.github.com>
2022-02-04 00:55:48 +0600

    started

--- set 2.py
+++ set 2.py
@@ -1,7 +1,56 @@
-class consumable:
+import pandas
 
-	def __init__(self, type):
-		self.type = type
+class consumable:
+	movies = list()
+	books = list()
+	series = list()
+	consumption_time = 0
+
+
+	text = ''' Enter: 1 for Books
+		2 for Movies
+		3 for Series
+
+		'''
+
+
+	def add_consumable(self):
+
+		choice = input(self.text)
+
+		def take_input():
+			name = input("\nEnter Book name: ")
+			rating = input("\nEnter rating: ")
+			days_consumed = input("\n Enter days consumed: ")
+			start_date = input("\n Enter Start date: ")
+			end_date = input("\n Enter End date: ")
+
+
+			keys = ["Name", "rating", "days_consumed", "start_date", "end_date"]
+
+			return(dict(zip(keys, [name, rating, days_consumed, start_date, end_date])))
+
+
+		if choice == '1':
+			#print(take_input())
+			consumable.books.append(take_input())
+		if choice == '2':
+			consumable.movies.append(take_input())
+		if choice == '3':
+			consumable.series.append(take_input())
+
+
+	def see_list(self):
+		choice = input(self.text)
+
+		if choice == '1':
+			#print(take_input())
+			print(consumable.books)
+		if choice == '2':
+			print(consumable.movies)
+		if choice == '3':
+			print(consumable.series)
+
 
 
 s = ''' Enter: 1. Add a consumable
@@ -11,4 +60,8 @@ s = ''' Enter: 1. Add a consumable
 		5. See overall info
 		'''
 
-option = input(s)
+#option = input(s)
+
+obj = consumable()
+obj.add_consumable()
+obj.see_list()
