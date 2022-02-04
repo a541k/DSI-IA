e7548a12f69cda597c5bbdc97393c8eb1afcf0e6
Ashik Mahmud <83023907+a541k@users.noreply.github.com>
2022-02-04 16:43:16 +0600

    edit woking

--- set 2.py
+++ set 2.py
@@ -2,7 +2,9 @@ class consumable:
 	movies = list()
 	books = list()
 	series = list()
-	consumption_time = 0
+	movie_consumption_time = 0
+	books_consumption_time = 0
+	series_consumption_time = 0
 
 
 	text = ''' Enter: 1 for Books
@@ -19,7 +21,7 @@ class consumable:
 		def take_input():
 			name = input("\nEnter name: ")
 			rating = input("\nEnter rating: ")
-			days_consumed = input("\nEnter days consumed: ")
+			days_consumed = int(input("\nEnter days consumed: "))
 			start_date = input("\nEnter Start date: ")
 			end_date = input("\nEnter End date: ")
 
@@ -40,12 +42,52 @@ class consumable:
 		choice = input(self.text)
 		name = input("\nEnter Product name to be edited: ")
 
+
+
 		def input_edits():
-			inc_consumed_time = input("\nEnter added hr's of consumption: ")
+			inc_consumed_time = int(input("\nEnter added hr's of consumption: "))
 			inc_days_consumed = input("\nAdd a day to days of consumption(1=Yes/0=No): ")
 			new_rating = input("\nNew rating: ")
 			end_date = input("\nEnter consumption ending date: ")
 			return(inc_consumed_time, inc_days_consumed, new_rating, end_date)
+
+		#def commit_edits():
+
+
+		if choice =='1':
+			inc_consumed_time, inc_days_consumed, new_rating, end_date = input_edits()
+
+			for index,item in enumerate(consumable.books):
+				if item.get("Name") == name:
+					if inc_days_consumed == "1":
+						consumable.books[index]["Days consumed"] += 1
+					if new_rating is not None:
+					    consumable.books[index]["Rating"] = new_rating
+					if end_date is not None:
+					    consumable.books[index]["end date"] = end_date
+		if choice =='2':
+			inc_consumed_time, inc_days_consumed, new_rating, end_date = input_edits()
+
+			for index,item in enumerate(consumable.movies):
+				if item.get("Name") == name:
+					if inc_days_consumed == "1":
+						consumable.movies[index]["Days consumed"] += 1
+					if new_rating is not None:
+					    consumable.movies[index]["Rating"] = new_rating
+					if end_date is not None:
+					    consumable.movies[index]["end date"] = end_date
+		if choice =='3':
+			inc_consumed_time, inc_days_consumed, new_rating, end_date = input_edits()
+
+			for index,item in enumerate(consumable.series):
+				if item.get("Name") == name:
+					if inc_days_consumed == "1":
+						consumable.series[index]["Days consumed"] += 1
+					if new_rating is not None:
+					    consumable.series[index]["Rating"] = new_rating
+					if end_date is not None:
+					    consumable.series[index]["end date"] = end_date
+
 
 
 
@@ -75,4 +117,7 @@ s = ''' Enter: 1. Add a consumable
 
 obj = consumable()
 obj.add_consumable()
+obj.see_list()
+
+obj.edit_consumable()
 obj.see_list()
